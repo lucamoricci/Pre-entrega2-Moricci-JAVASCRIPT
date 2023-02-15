8e5f9730a5d1afbbe9ea212d193acd87c84b6fe9
lucamoricci <lucamoricci3@gmail.com>
2023-02-15 10:13:46 -0300

    Pre-Entrega2-Moricci

---
+++ main.js
@@ -0,0 +1,96 @@
+
+function operaciones(op)
+{
+
+    let ops = {
+        sumar: function sumarNumeros(n1, n2) {
+            return (parseInt(n1) + parseInt(n2));
+        },
+
+        restar: function restarNumeros(n1, n2) {
+            return (parseInt(n1) - parseInt(n2));
+        },
+        
+        multiplicar: function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));
+        },
+
+        dividir: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+
+    };
+
+    let operacion;
+    let operando1;
+
+    switch(op) {
+        
+        case 'sumar':
+            operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "+";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "suma";
+            break;
+        case 'restar':
+            operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "-";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "resta";
+            break;
+        case 'multiplicar':
+            operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "*";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "multiplicacion";
+            break;
+        case 'dividir':
+            operando1 = document.getElementById("resultado").value;
+            document.getElementById("resultado").value = operando1 + "/";
+            operacion = document.getElementById("resultado").value;
+            document.getElementById("memoria").value = "division";
+            break;
+            
+        case 'igual':	
+            
+            operacion = document.getElementById("resultado").value;
+            
+            var memoriaop = document.getElementById("memoria").value;
+
+
+            let operandos;
+            let resultado;
+            
+            switch(memoriaop) {
+                case 'suma':
+                 operandos = operacion.split("+");
+                resultado = ops.sumar(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+                case 'resta':
+                 operandos = operacion.split("-");
+                 resultado = ops.restar(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+                case 'multiplicacion':
+                 operandos = operacion.split("*");
+                 resultado = ops.multiplicar(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;
+                case 'division':
+                 operandos = operacion.split("/");
+                 resultado = ops.dividir(operandos[0], operandos[1]);
+                    document.getElementById("resultado").value = resultado;
+                    break;					
+            
+            }	
+            
+
+            break;
+            
+
+    }
+
+
+}
